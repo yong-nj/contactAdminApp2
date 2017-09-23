67010a73d5462816ef21f7f02179e89bc30adfde
yong-NJ <yhuang213@hotmail.com>
2017-09-23 13:09:38 -0400

    Add files via upload

---
+++ app/ContactForm/cfController.js
@@ -0,0 +1,71 @@
+﻿contactAdminApp2.controller('cfController',
+    ["$scope",  "$window", "$routeParams", "DataService",
+    function cfController($scope,  $window, $routeParams, DataService) {
+
+        $scope.editableContact = {};
+
+        if ($routeParams.id) {
+            $scope.contact = DataService.getContact($routeParams.id).then(
+               function (results) {
+                   // on success
+                   $scope.contact = results.data[0];                 
+                   $scope.editableContact = angular.copy($scope.contact);
+               },
+               function (results) {
+                   // on error
+                   $scope.contacts = results.data;
+               });
+        }
+        else {
+            $scope.contact = { id: -1 };
+        };
+
+
+       
+
+        $scope.cancelForm = function () {
+            $window.history.back();
+        }
+
+        $scope.submitForm = function () {
+            $scope.$broadcast('show-errors-event');
+            //  if ($scope.contactForm.$invalid)    return;
+
+            //today date
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = ("0" + (today.getMonth() + 1)).slice(-2); //January is 0!
+            var yyyy = today.getFullYear();
+            //end today date
+            if ($scope.editableContact.birthDate == undefined) $scope.editableContact.birthDate = yyyy + '-' + mm + '-' + dd;
+
+            if ($scope.editableContact.id == undefined) {              
+                DataService.insertContact($scope.editableContact).then(
+                    function (results) {
+                        // on success
+                        $scope.contact = angular.copy($scope.editableContact);
+                        $window.history.back();
+                    },
+                    function (results) {
+                        // on error
+                        $scope.hasFormError = true;
+                        $scope.formErrors = results.statusText;
+                    });
+
+            }
+            else {
+                var r = confirm("Are you sure you want to update this contact?  click Ok to update this contact, Click cancel to cancel.");
+                if (r == true) {
+                    DataService.updateContact($scope.editableContact);
+                    $window.location.href = "/";
+                } else {
+                    // txt = "You pressed Cancel!";
+                }
+            
+              
+               
+            }
+           
+        }
+
+    }]);
---
+++ app/ContactForm/cfDirective.js
@@ -0,0 +1,25 @@
+﻿
+contactAdminApp2.directive('contactForm',
+    function () {
+        
+        return {
+            restrict: 'E',
+            templateUrl: 'app/ContactForm/cfContact.html'
+        }
+
+    });
+
+
+//angular.module('contactAdminApp2')
+//    .directive('uibDatepickerPopup', function () {
+//        return {
+//            restrict: 'EAC',
+//            require: 'ngModel',
+//            link: function (scope, elem, attrs, ngModel) {
+//                ngModel.$parsers.push(function toModel(date) {
+//                    //  return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+//                    return '1988-01-01';
+//                });
+//            }
+//        }
+//    });
